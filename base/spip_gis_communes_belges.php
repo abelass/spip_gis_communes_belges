4cb99d6e7ffbac5d9c58d412ea30f12466ef3bab
Rainer <rainer@websimple.be>
2017-05-20 11:47:32 +0200

    import des donnés
    - encore erreur "Cannot get geometry object from data you send to the
    GEOMETRY field"

---
+++ base/spip_gis_communes_belges.php
@@ -0,0 +1,31 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+function gis_cb_importer_points() {
+	$json = json_decode(file_get_contents(__DIR__ . '/../data/complete_data.json'), TRUE);
+
+	$set = array();
+	foreach($json AS $values) {
+		$nom_fr = $values['Name']['fr'];
+		$nom_nl = isset($values['Name']['nl']) ? $values['Name']['nl'] : $nom_fr;
+		$polygon = array('type' => 'Feature','geometry' => $values['POLYGON']);
+		$wkt = json_to_wkt(json_encode($polygon));
+		$set = array(
+			'titre' => '<multi>[fr]' . $nom_fr. '[nl]' . $nom_nl. '</multi>',
+			'lat' => $values['lat'],
+			'lon' => $values['lng'],
+			'geo' => "ST_GeomFromText('$wkt')",
+			'zoom' => 11,
+			'type' => 'Multipolygon',
+			'pays' => '<multi>[fr]Belgique[nl]België</multi>',
+			'code_pays' => 'BE',
+			'ville' => '<multi>[fr]' . $nom_fr. '[nl]' . $nom_nl. '</multi>',
+			'code_postal' => $values['zip'],
+			);
+	}
+	sql_insertq_multi('spip_gis' , $set);
+}
+
